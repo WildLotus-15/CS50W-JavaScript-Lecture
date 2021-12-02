7b8da60d28d08e99e820959fb40e09f056d241d1
unknown <msnoopy50@gmail.com>
2021-12-02 19:06:06 +0400

    shit is getting hard + shit is getting interesting = pizdec

--- counter.js
+++ counter.js
@@ -1,7 +1,18 @@
-let counter = 0
+// the shit is real so listen
+// JavaScript isn't saving any state of web-site by default specifically storage of it for example values of the variables
+// in order to change that we can use local storage syntax
+// the ! syntax means "not" in that case if local storage can't get counter item because it doesn't exist
+
+/* let counter = 0 */
+
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0)
+}
 function count() {
+    let counter = localStorage.getItem('counter')
     counter++; //this means take the old value of counter and increment it by 1 (counter += 1 in python; counter = counter + 1)
     document.querySelector('h1').innerHTML = counter
+    localStorage.setItem('counter', counter)
 
     if (counter % 10 === 0) {
         alert(`Count is now ${counter}`) // this is a literal template where value of attribute plugs in -> `${}`
@@ -12,5 +23,7 @@ function count() {
     // if we will run function before loading DOMContent error will be shown on web-site JavaScript terminal               
 }
 document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter')
     document.querySelector('button').onclick = count /*document.querySelector('button').addEventListener('click', count) would do the same*/
+    /* setInterval(count, 1000) */ // <- there we are setting interval which will run specific function in every specific milliseconds 
 })
